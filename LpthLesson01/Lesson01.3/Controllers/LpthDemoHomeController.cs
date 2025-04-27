ab6eb35663263d44ea139eaf62400ff1dd8e4502
lephantrunghieu <lephantrunghieu555@gmail.com>
2025-04-27 13:22:54 +0700

    add

---
+++ LpthLesson01/Lesson01.3/Controllers/LpthDemoHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01._3.Controllers
+{
+    public class LpthDemoHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
