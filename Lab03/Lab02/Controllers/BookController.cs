36d215b75f438da18baa34646d113b962ae7f2aa
lephantrunghieu <lephantrunghieu555@gmail.com>
2025-05-18 17:01:03 +0700

    add

---
+++ Lab03/Lab02/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab03.Models;
+namespace Lab03.Controllers
+{
+    public class BookController : Controller
+    {
+        protected Book book = new Book();
+
+        public IActionResult Index()
+        {
+            // danh sách genres convert SelectListItem để hiển thị trên combobox
+            ViewBag.authors = book.Authors; // truyền dữ liệu SelectListItem qua view
+            ViewBag.genres = book.Genres;   // truyền dữ liệu SelectListItem qua view
+            var books = book.GetBookList();
+            return View(books); // truyền dữ liệu qua view dưới dạng tham số
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.authors = book.Authors;  // truyền dữ liệu SelectListItem qua view
+            ViewBag.genres = book.Genres;    // truyền dữ liệu SelectListItem qua view
+            Book model = new Book();
+            return View(model);
+        }
+
+
+    }
+}
---
+++ Lab03/Lab02/Models/Book.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lab03.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public int TotalPage { get; set; }
+        public string? Sumary { get; set; }
+
+        // danh sách các cuốn sách (nhớ using System.Collections.Generic)
+        public List<Book> GetBookList()
+        {
+            List<Book> books = new List<Book>()
+    {
+        new Book()
+        {
+            Id = 1,
+            Title = "Chí Phèo",
+            AuthorId = 1,
+            GenreId = 1,
+            Image = "/images/products/b1.jpg",
+            Price = 500000,
+            Sumary = "",
+            TotalPage = 250
+        },
+        new Book()
+        {
+            Id = 2,
+            Title = "Nam Cao",
+            AuthorId = 1,
+            GenreId = 1,
+            Image = "/images/products/b2.jpg",
+            Price = 500000,
+            Sumary = "",
+            TotalPage = 250
+        },
+        new Book()
+        {
+            // Thêm sách khác tại đây nếu cần
+        },
+        new Book()
+        {
+            // Thêm sách khác tại đây nếu cần
+        }
+    };
+
+            return books;
+        }
+        //chi tiết 1 cuốn sách theo id 
+        public Book GetBookById(int id)
+        {
+            Book book = this.GetBookList().FirstOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        // SelectListItem Authors (using Microsoft.AspNetCore.Mvc.Rendering)
+        public List<SelectListItem> Authors { get; } = new List<SelectListItem>
+        {
+        new SelectListItem { Value = "1", Text = "Nam cao" },
+        new SelectListItem { Value = "2", Text = "Ngô Tất Tố" },
+        new SelectListItem { Value = "3", Text = "Adamkhoom" },
+        new SelectListItem { Value = "4", Text = "Thiền sư Thích Nhất Hạnh" }
+        };
+
+        // SelectListItem Genres
+        public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+        {
+        new SelectListItem { Value = "1", Text = "Truyện tranh" },
+        new SelectListItem { Value = "2", Text = "Văn học đương đại" },
+        new SelectListItem { Value = "3", Text = "Phật học phổ thông" },
+        new SelectListItem { Value = "4", Text = "Truyện cười" }
+        };
+
+    }
+}
